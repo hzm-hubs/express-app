62ad467bd85f36517a752226a7f055076126ba6f
hzm <467251648@qq.com>
2023-06-01 15:07:58 +0800

    feat: 添加花哨的启动打印

---
+++ plugins/os.js
@@ -0,0 +1,30 @@
+const { networkInterfaces, cpus } = require("os");
+var consola = require("consola");
+
+// 获取本地IP
+function getIP(typeList = ["lo0", "lo", "en0", "eth0"]) {
+	const ipObj = networkInterfaces();
+	if (typeList?.length) {
+		const results = {};
+		typeList.forEach((item) => {
+			results[item] = {};
+			ipObj[item]?.forEach((it) => {
+				results[item][it.family] = it.address;
+			});
+			// consola.info("results", results);
+		});
+		return results;
+	} else {
+		return ipObj;
+	}
+}
+
+// 获取CPU
+function getCPU(type = "en0") {
+	return cpus();
+}
+
+module.exports = {
+	getIP,
+	getCPU,
+};
