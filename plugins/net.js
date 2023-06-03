ab4fb6524401b909cd0591a067f81684ed5bf28d
hzm <467251648@qq.com>
2023-06-03 20:48:38 +0800

    feat: 添加端口是否可用校验

--- plugins/net.js
+++ plugins/net.js
@@ -3,55 +3,66 @@ const consola = require("consola");
 const net = require("net");
 
 // 检查传入的port是否已经在使用
-function tryConnectPort(port) {
-	consola.info("传入的端口号", port);
-	const theServer = net.createServer();
-	// return new Promise((resolve, reject) => {
-	// 启动监听连接的服务器
-	// consola.log("theServer", theServer);
-	// 当服务器开始监听时，将触发 'listening' 事件。 最后一个参数 callback 将被添加为 'listening' 事件的监听器。
-	theServer.on("listening", function () {
-		setTimeout(() => {
-			if (theServer) {
-				theServer.close();
-			}
-		}, 3000);
-	});
+function tryConnectPort({ port, host }) {
+  // consola.info("传入的端口号", port);
+  const theServer = net.createServer();
+  // consola.log("theServer", theServer);
+  return new Promise((resolve, reject) => {
+    // 启动监听连接的服务器
+    // 当服务器开始监听时，将触发 'listening' 事件。 最后一个参数 callback 将被添加为 'listening' 事件的监听器。
+    theServer.on("listening", function () {
+      setTimeout(() => {
+        // 没有监听到使用就主动关闭
+        theServer.close();
+        resolve({
+          port,
+          nouse: true,
+          from: "listening",
+        });
+      }, 3000);
+    });
 
-	theServer.on("error", (e) => {
-		consola.log("监听出发error", e);
-		if (e?.code === "EADDRINUSE") {
-			consola.log("port正在使用", e);
-			resolve({
-				port,
-				nouse: false,
-				from: "EADDRINUSE",
-			});
-		} else {
-			resolve({
-				port,
-				nouse: true,
-				from: "else",
-			});
-		}
-	});
+    theServer.on("error", (e) => {
+      //   consola.log("监听出发error", e);
+      if (e?.code === "EADDRINUSE") {
+        // consola.log("port正在使用", e);
+        resolve({
+          port,
+          nouse: false,
+          from: "error-EADDRINUSE",
+        });
+      } else {
+        resolve({
+          port,
+          nouse: true,
+          from: "error-common",
+        });
+      }
+    });
 
-	theServer.listen(3000);
-	// });
+    // 需要传入host才能成功监听是否有在使用
+    theServer.listen({
+      port,
+      host,
+    });
+  });
 }
-function createPort(port, sucBack = function () {}) {
-	tryConnectPort(port);
-	// tryConnectPort(port).then((backData) => {
-	// 	consola.info("backData", backData);
-	// 	if (backData?.nouse) {
-	// 		sucBack({ port, ...arguments });
-	// 		return port;
-	// 	} else {
-	// 		// createPort(port + 1, sucBack);
-	// 	}
-	// });
+
+function createPort({ port, host }, callback = function () {}) {
+  return tryConnectPort({ port, host }).then((backData) => {
+    // consola.info("backData", port, backData);
+    if (backData?.nouse) {
+      // 成功找到port后的回调
+      if (callback) {
+        callback({ port, host, ...arguments });
+      }
+      return port;
+    } else {
+      createPort({ port: port + 1, host }, callback);
+    }
+  });
 }
 
 module.exports = {
-	createPort,
+  createPort,
 };
--- plugins/os.js
+++ plugins/os.js
@@ -3,28 +3,29 @@ var consola = require("consola");
 
 // 获取本地IP
 function getIP(typeList = ["lo0", "lo", "en0", "eth0"]) {
-	const ipObj = networkInterfaces();
-	if (typeList?.length) {
-		const results = {};
-		typeList.forEach((item) => {
-			results[item] = {};
-			ipObj[item]?.forEach((it) => {
-				results[item][it.family] = it.address;
-			});
-			// consola.info("results", results);
-		});
-		return results;
-	} else {
-		return ipObj;
-	}
+  const ipObj = networkInterfaces();
+  //   consola.log("ipObj", ipObj);
+  if (typeList?.length) {
+    const results = {};
+    typeList.forEach((item) => {
+      results[item] = {};
+      ipObj[item]?.forEach((it) => {
+        results[item][it.family] = it.address;
+      });
+      //   consola.info("results", results);
+    });
+    return results;
+  } else {
+    return ipObj;
+  }
 }
 
 // 获取CPU
 function getCPU(type = "en0") {
-	return cpus();
+  return cpus();
 }
 
 module.exports = {
-	getIP,
-	getCPU,
+  getIP,
+  getCPU,
 };
