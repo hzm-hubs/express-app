e47976495b7ef5e69ada35a52f45b2a1b67dceac
hzm <467251648@qq.com>
2023-06-24 17:06:46 +0800

    feat: 尝试使用fs模块

---
+++ plugins/path.js
@@ -0,0 +1,2 @@
+// 路径模块
+const path = require("path");
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var { readFile } = require("../plugins/fs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  readFile("./static/滕王阁序.txt")
+    .then((read) => {
+      res.send(read);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 module.exports = router;
