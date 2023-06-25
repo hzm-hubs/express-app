39b38e0751c1e12dce0b4d2284b2994504f807d2
hzm <467251648@qq.com>
2023-06-25 18:54:52 +0800

    feat: 了解next用法

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.get(
+	"/",
+	function (req, res, next) {
+		// next() 是一个中间件函数中的参数，用于将控制权传递给下一个中间件函数或路由处理程序，使它们按照定义的顺序依次执行。
+		console.log("1");
+		next();
+	},
+	function (req, res, next) {
+		console.log("2");
+		next();
+	},
+	function (req, res) {
+		console.log("3");
+		res.render("index", { title: "Express" });
+	}
+);
 
 module.exports = router;
